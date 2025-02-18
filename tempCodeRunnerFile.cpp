bb2ea493c135a6e69aeede9ddc3699aee0b93883
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2025-02-18 15:41:07 +0700

    lab21-1 completed

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main(int argc,char *argv[]) {
     double a=0;
-    if (argc > 1 ) {
-        for (int i=1;i<argc;i++){
+    if (argc != 0 ) {
+        for (int i=1;i<=argc;i++){
             a += atof(argv[i]);
         }
         cout << "---------------------------------\n";
         cout << "Average of " << argc-1 << " number = " << a/double(argc-1) << "\n";
         cout << "---------------------------------\n";
-        
+        return 0;
     }else {
         cout << "Please input numbers to find average.";
     }
-    return 0;
+    
 }
